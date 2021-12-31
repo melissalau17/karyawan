74e98445e47187e5a508675185e81d3ce064b170
riki aldi pari <rikialdipari@gmail.com>
2021-12-31 20:12:08 +0700

    soal tiga

--- src/main/java/com/investree/demo/repository/TransaksiRepository.java
+++ src/main/java/com/investree/demo/repository/TransaksiRepository.java
@@ -2,10 +2,14 @@ package com.investree.demo.repository;
 
 
 import com.investree.demo.model.Transaksi;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface TransaksiRepository extends PagingAndSortingRepository<Transaksi, Long> {
+    @Query("select c from Transaksi c WHERE c.id = :id")
+    public Transaksi getByID(@Param("id") Long id);
 }
